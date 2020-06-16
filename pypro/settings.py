592045c6310aa33ff0f3d1f3df0bb67190a1a428
Val Araújo <val.spminas@gmail.com>
2020-06-16 17:56:12 -0300

    Implementada Integração com Sentry SDK close #39

--- pypro/settings.py
+++ pypro/settings.py
@@ -17,6 +17,8 @@ import os
 import dj_database_url
 from decouple import config, Csv
 from functools import partial
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -170,3 +172,7 @@ if AWS_ACCESS_KEY_ID:
     INSTALLED_APPS.append('s3_folder_storage')
 
     INSTALLED_APPS.append('storages')
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()])
