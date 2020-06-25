ee5f390481671bb3f2b5434cb64b5591636fd787
Val Araújo <val.spminas@gmail.com>
2020-06-25 12:50:01 -0300

    implementado banco de dados postgres com Docker e testes com banco de dados close #70

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -16,6 +16,7 @@ def video(db):
 def resp(client, video):
     return client.get(reverse('aperitivos:video', args=(video.slug,)))
 
+
 @pytest.fixture
 def resp_video_nao_encontrado(client, video):
     return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
@@ -23,6 +24,7 @@ def resp_video_nao_encontrado(client, video):
 
 def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
     assert resp_video_nao_encontrado.status_code == 404
+
 
 def test_status_code(resp):
     assert resp.status_code == 200
